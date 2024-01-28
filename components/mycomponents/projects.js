7f17b6eb14be4ee3cecb48ffecb890b197e6e6d2
Faareh-Ahmed <faarehahmed@gmail.com>
2024-01-28 21:47:17 +0500

    Improved Styling for Project section

--- components/mycomponents/projects.js
+++ components/mycomponents/projects.js
@@ -26,11 +26,11 @@ export default function Projects() {
                                 />
                                 <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
                                     <h3>
-                                        <a href="javascript:void(0)" className="font-semibold text-dark text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-4 block hover:text-primary " >
+                                        <a href="javascript:void(0)" className="font-semibold text-foreground text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-4 block hover:text-primary " >
                                             The Flags
                                         </a>
                                     </h3>
-                                    <p className="text-base text-body-color leading-relaxed mb-7">
+                                    <p className="text-base text-body-color leading-relaxed mb-7 text-foreground">
                                         Discover the world with 'The Flags,' a ReactJS web app. Immerse yourself in diverse country details powered by a RESTful API and Axios. Explore demographics, languages, and currencies in real time. Tailor your journey using region filters and search options                                    </p>
                                     <Button href="javascript:void(0)" className=" inline-block py-2 px-7 border border-ring rounded-full text-background bg-primary  font-medium  transition ">
                                         View on GitHub
@@ -39,7 +39,7 @@ export default function Projects() {
                             </div>
                         </div>
                         <div className="w-full md:w-1/2 xl:w-1/3 px-4 mb-8">
-                            <div className="bg-white rounded-lg overflow-hidden mb-10 h-full transform hover:scale-105 transition-transform ">
+                            <div className="bg-background rounded-lg overflow-hidden mb-10 h-full transform hover:scale-105 transition-transform ">
                                 <Image
                                     src={MediCare}
                                     alt="image"
@@ -47,11 +47,11 @@ export default function Projects() {
                                 />
                                 <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
                                     <h3>
-                                        <a href="javascript:void(0)" className=" font-semibold text-dark text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-4  block hover:text-primary">
+                                        <a href="javascript:void(0)" className=" font-semibold text-foreground text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-4  block hover:text-primary">
                                             MediCare
                                         </a>
                                     </h3>
-                                    <p className="text-base text-body-color leading-relaxed mb-7">
+                                    <p className="text-base text-body-color leading-relaxed mb-7 text-foreground">
                                         Developed a comprehensive Hospital Management System leveraging the MERN Stack. This system is designed to streamline and simplify hospital operations, focusing on efficient staff management, seamless patient registration, and appointment scheduling.                                    </p>
                                     <Button href="javascript:void(0)" className=" inline-block py-2 px-7 border border-ring rounded-full text-background bg-primary  font-medium  transition ">
                                         View on GitHub
@@ -60,7 +60,7 @@ export default function Projects() {
                             </div>
                         </div>
                         <div className="w-full md:w-1/2 xl:w-1/3 px-4 mb-8">
-                            <div className="bg-white rounded-lg overflow-hidden mb-10 h-full transform hover:scale-105 transition-transform ">
+                            <div className="bg-background rounded-lg overflow-hidden mb-10 h-full transform hover:scale-105 transition-transform ">
                                 <Image
                                     src={LittleLemon2}
                                     alt="image"
@@ -68,11 +68,11 @@ export default function Projects() {
                                 />
                                 <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
                                     <h3>
-                                        <a href="javascript:void(0)" className="  font-semibold  text-dark text-xl  sm:text-[22px]  md:text-xl  lg:text-[22px]  xl:text-xl  2xl:text-[22px]  mb-4  block  hover:text-primary  ">
+                                        <a href="javascript:void(0)" className="  font-semibold  text-foreground text-xl  sm:text-[22px]  md:text-xl  lg:text-[22px]  xl:text-xl  2xl:text-[22px]  mb-4  block  hover:text-primary  ">
                                             Little Lemon Restaurant
                                         </a>
                                     </h3>
-                                    <p className="text-base text-body-color leading-relaxed mb-7">
+                                    <p className="text-base text-body-color leading-relaxed mb-7 text-foreground">
                                         Developed an engaging and interactive front end for a restaurant website using React. This project showcases my proficiency in creating dynamic and visually appealing user interfaces, providing a seamless and delightful user experience.                                    </p>
                                     <Button href="javascript:void(0)" className=" inline-block py-2 px-7 border border-ring rounded-full text-background bg-primary  font-medium  transition ">
                                         View on GitHub
