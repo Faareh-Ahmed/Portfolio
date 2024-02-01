2035efdad2f9d566dae38f669c378717ad46f071
Faareh-Ahmed <faarehahmed@gmail.com>
2024-02-01 23:12:48 +0500

    Project section Links and About responsiveness

--- components/mycomponents/projects.js
+++ components/mycomponents/projects.js
@@ -26,13 +26,13 @@ export default function Projects() {
                                 />
                                 <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
                                     <h3>
-                                        <a href="javascript:void(0)" className="font-semibold text-foreground text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-4 block hover:text-primary " >
+                                        <a href="https://github.com/Faareh-Ahmed/The-Flags" target="blank"  rel="noopener noreferrer" className="font-semibold text-foreground text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-4 block hover:text-primary " >
                                             The Flags
                                         </a>
                                     </h3>
                                     <p className="text-base text-body-color leading-relaxed mb-7 text-foreground">
                                         Discover the world with 'The Flags,' a ReactJS web app. Immerse yourself in diverse country details powered by a RESTful API and Axios. Explore demographics, languages, and currencies in real time. Tailor your journey using region filters and search options                                    </p>
-                                    <Button href="javascript:void(0)" className=" inline-block py-2 px-7 border border-ring rounded-full text-background bg-primary  font-medium  transition ">
+                                    <Button onClick={() => window.open("https://github.com/Faareh-Ahmed/The-Flags", "_blank")} className=" inline-block py-2 px-7 border border-ring rounded-full text-background bg-primary  font-medium  transition ">
                                         View on GitHub
                                     </Button>
                                 </div>
@@ -47,13 +47,13 @@ export default function Projects() {
                                 />
                                 <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
                                     <h3>
-                                        <a href="javascript:void(0)" className=" font-semibold text-foreground text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-4  block hover:text-primary">
+                                        <a href="https://github.com/Faareh-Ahmed/Hospital-Management-System" target="blank"  rel="noopener noreferrer" className=" font-semibold text-foreground text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-4  block hover:text-primary">
                                             MediCare
                                         </a>
                                     </h3>
                                     <p className="text-base text-body-color leading-relaxed mb-7 text-foreground">
                                         Developed a comprehensive Hospital Management System leveraging the MERN Stack. This system is designed to streamline and simplify hospital operations, focusing on efficient staff management, seamless patient registration, and appointment scheduling.                                    </p>
-                                    <Button href="javascript:void(0)" className=" inline-block py-2 px-7 border border-ring rounded-full text-background bg-primary  font-medium  transition ">
+                                    <Button onClick={() => window.open("https://github.com/Faareh-Ahmed/Hospital-Management-System", "_blank")} className=" inline-block py-2 px-7 border border-ring rounded-full text-background bg-primary  font-medium  transition ">
                                         View on GitHub
                                     </Button>
                                 </div>
@@ -68,13 +68,13 @@ export default function Projects() {
                                 />
                                 <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
                                     <h3>
-                                        <a href="javascript:void(0)" className="  font-semibold  text-foreground text-xl  sm:text-[22px]  md:text-xl  lg:text-[22px]  xl:text-xl  2xl:text-[22px]  mb-4  block  hover:text-primary  ">
+                                        <a href="https://github.com/Faareh-Ahmed/Little-Lemon-Capstone-Project" target="blank"  rel="noopener noreferrer" className="  font-semibold  text-foreground text-xl  sm:text-[22px]  md:text-xl  lg:text-[22px]  xl:text-xl  2xl:text-[22px]  mb-4  block  hover:text-primary  ">
                                             Little Lemon Restaurant
                                         </a>
                                     </h3>
                                     <p className="text-base text-body-color leading-relaxed mb-7 text-foreground">
                                         Developed an engaging and interactive front end for a restaurant website using React. This project showcases my proficiency in creating dynamic and visually appealing user interfaces, providing a seamless and delightful user experience.                                    </p>
-                                    <Button href="javascript:void(0)" className=" inline-block py-2 px-7 border border-ring rounded-full text-background bg-primary  font-medium  transition ">
+                                    <Button onClick={() => window.open("https://github.com/Faareh-Ahmed/Little-Lemon-Capstone-Project", "_blank")} className=" inline-block py-2 px-7 border border-ring rounded-full text-background bg-primary  font-medium  transition ">
                                         View on GitHub
                                     </Button>
                                 </div>
